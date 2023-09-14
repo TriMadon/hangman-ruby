572d4401a15f9561b4aa8a7e78b4cfd6a1ac262a
TriMadon <mehammed6789@gmail.com>
2023-09-14 18:07:55 +0300

    Resolve #5; Add welcome message

--- hangman_game.rb
+++ hangman_game.rb
@@ -12,15 +12,37 @@ class HangmanGame
     @chosen_word = nil
     @rem_incorr_guesses = nil
     @incorrect_letters = nil
+
+    welcome_message
   end
 
   def start
+    puts "let's get started!"
     @chosen_word = Word.new(random_word)
     @rem_incorr_guesses = 7
     @incorrect_letters = []
   end
 
   private
+
+  def welcome_message
+    puts '╔════════════════════════════════════════════════╗'
+    puts '║                   Welcome to                   ║'
+    puts '║                    Hangman!                    ║'
+    puts "╚════════════════════════════════════════════════╝\n\n"
+    puts "Hangman is a word-guessing game. Here's how to play:\n\n"
+    puts instructions
+  end
+
+  def instructions
+    puts <<-INSTRUCTIONS
+    1. We'll choose a secret word, and your goal is to guess it letter by letter.
+    2. You have 6 incorrect guesses allowed.
+    3. Type a letter and press Enter to make a guess.
+    4. If you guess a correct letter, it will be revealed in the word.
+    5. If you guess an incorrect letter, it will be added to your incorrect letters.
+    INSTRUCTIONS
+  end
 
   def load_dictionary
     IO.readlines(DICTIONARY_FILE, chomp: true)
