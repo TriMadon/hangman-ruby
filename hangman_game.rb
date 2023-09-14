7a00da79c3cdc31d9a94cba839e1031b371e04f5
TriMadon <mehammed6789@gmail.com>
2023-09-14 16:01:20 +0300

    Make base classes

---
+++ hangman_game.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require './word'
+
+class HangmanGame
+  def initialize
+
+  end
+end
---
+++ main.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require './hangman_game'
+
+game = HangmanGame.new
+game.start
